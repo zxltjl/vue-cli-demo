c8ebdc260f665d098abe83a4bf782736c0adc341
tjl <tjlzxl@163.com>
2020-01-12 18:05:04 +0800

    路由验权

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 // import db from '@/libs/db'
+import store from '../store';
 import NProgress from 'nprogress';
 import '../assets/style/nprocess.less';//引入自定义的nprocess样式
 Vue.use(VueRouter);
@@ -34,14 +35,40 @@ const router = new VueRouter({
             path:'/user',
             name:'Login',
             meta:{
-                title:'登录'
+                title:'登录',
+                notAuth: true,
+                notCache: true
             },
+            
             component:()=>import('@/Components/layout/userLayout.vue')
         },
     ]
 });
-router.beforeEach((to, from, next) => {
+
+
+const isRouterAuth = function (to, from, next) {
+    const isAuth = to.matched.some(r => !r.meta.notAuth);
+    return isAuth || next();
+};
+
+//第一次进入
+const isFirstEnter = async function (to, from, next) {
+    if (!store.state.user.token) {
+        if(to.name==='Login'){
+            next();
+        }else{
+            return next({ name: 'Login' });
+        }
+    }
+    return true;
+};
+
+
+
+router.beforeEach(async (to, from, next) => {
     NProgress.start();
+    if (!isRouterAuth(to,from,next)) return;
+    if (!await isFirstEnter(to, from, next)) return;
     next()
     /* 必须调用 `next` */
 });
@@ -51,9 +78,11 @@ router.beforeResolve((to, from, next) => {
 /* 必须调用 `next` */
 });
 
-router.afterEach(() => {
-    NProgress.done();
-    // console.log(to);
-    // console.log(from);
+router.afterEach((to) => {
+    Promise.resolve().then(() => {
+        NProgress.done();
+    });
+    window.document.title = to.meta.title ;
+    window.scrollTo(0, 0);
 });
 export default router;
